3d7130cb52fe487936ac7065030e4b383bf6ccd4
amen <zju3020912063@gmail.com>
2014-12-08 16:48:18 +0800

    Signed-off-by: amen <zju3020912063@gmail.com>

--- cloud/main.js
+++ cloud/main.js
@@ -1,6 +1,3 @@
-require("cloud/app.js");
-// Use AV.Cloud.define to define as many cloud functions as you want.
-// For example:
 var mqiniu=require('cloud/mqiniu');
 
 AV.Cloud.define("qiniuUptoken",mqiniu.qiniuUptoken);
