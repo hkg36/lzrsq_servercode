c731eac28163cf377b094aaff3e10a8eb9089f1a
amen <zju3020912063@gmail.com>
2014-12-09 15:48:25 +0800

    Signed-off-by: amen <zju3020912063@gmail.com>

--- cloud/CurriculumVitaeReview.js
+++ cloud/CurriculumVitaeReview.js
@@ -5,21 +5,21 @@ var CurriculumVitae=AV.Object.extend("CurriculumVitae")
 var CurriculumVitaeReview=AV.Object.extend("CurriculumVitaeReview")
 function AfterSave(request){
     cvc=request.object
-    cvid=cvc.get("cvid")
-    uid=cvc.get("uid")
+    cv=cvc.get("cv")
+    user=cvc.get("user")
 
     var cvrlist_len=0
     var cvuserlist=null
     var tagslist={}
     var cvrquery=new AV.Query(CurriculumVitaeReview);
-    cvrquery.equalTo("cvid",cvid)
+    cvrquery.equalTo("cv",cv)
     cvrquery.find({
       success: function(results) {
           cvrlist_len=results.length
           var tmpuids={}
           for (var i = 0; i < results.length; i++) {
               var cvc = results[i];
-              tmpuids[cvc.get("uid")]=true
+              tmpuids[cvc.get("user")]=true
               var tags=cvc.get("tags")
               for(var tagi=0;tagi<tags.length;tagi++)
               {
@@ -37,21 +37,10 @@ function AfterSave(request){
       error: function(error) {
       }
     })
-    var query = new AV.Query(CurriculumVitae);
-    query.equalTo("objectId", cvid);
-    query.find({
-      success: function(results) {
-        if(results.length==1){
-            cv=results[0]
-        }
-          cv.addUnique("comment_uids",uid)
-          cv.set("comment_count",cvrlist_len)
-          cv.set("tags",tagslist)
-          cv.save()
-      },
-      error: function(error) {
-      }
-    })
+      cv.set(("comment_users",cvuserlist)
+      cv.set("comment_count",cvrlist_len)
+      cv.set("tags",tagslist)
+      cv.save()
 }
 
 exports.AfterSave=AfterSave
