2d29e8c81415533c413aa0ae107b0a843342cbf7
amen <zju3020912063@gmail.com>
2014-12-09 17:18:59 +0800

    Signed-off-by: amen <zju3020912063@gmail.com>

--- cloud/CurriculumVitaeReview.js
+++ cloud/CurriculumVitaeReview.js
@@ -3,6 +3,14 @@
  */
 var CurriculumVitae=AV.Object.extend("CurriculumVitae")
 var CurriculumVitaeReview=AV.Object.extend("CurriculumVitaeReview")
+function allvalues(obj)
+{
+  pp=new Array()
+  for(var key in kk) {
+    pp.push(kk[key])
+  }
+  return pp
+}
 function AfterSave(request){
     var cvc=request.object
     var cv=cvc.get("cv")
@@ -13,14 +21,15 @@ function AfterSave(request){
     cvrquery.find({
       success: function(results) {
           var cvrlist_len=0
-            var cvuserlist=new Array()
+            var cvuserlist={}
             var tagslist={}
           console.log("cur:"+results)
           cvrlist_len=results.length
           var tmpuids={}
           for (var i = 0; i < results.length; i++) {
               var cvc = results[i];
-              cvuserlist.push(cvc.get("user"))
+              var oneuser=cvc.get("user")
+              cvuserlist[oneuser.id]=oneuser
               var tags=cvc.get("tags")
               for(var tagi=0;tagi<tags.length;tagi++)
               {
@@ -33,15 +42,11 @@ function AfterSave(request){
                   }
               }
           }
-         // var cvrquery=new AV.Query(CurriculumVitae).get(cv.id,{
-         //     success:function(cv_save){
-          cv_save=cv
-                  cv_save.set("comment_users",cvuserlist)
-                  cv_save.set("comment_count",cvrlist_len)
-                  cv_save.set("tags",tagslist)
-                  cv_save.save()
-        //      }
-         // });
+          cvuserlist=allvalues(cvuserlist)
+          cv.set("comment_users",cvuserlist)
+          cv.set("comment_count",cvrlist_len)
+          cv.set("tags",tagslist)
+          cv.save()
       },
       error: function(error) {
       }
