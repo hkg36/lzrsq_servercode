485164bfc2da50108140f477a29f6a9f8835266b
amen <zju3020912063@gmail.com>
2014-12-11 17:10:07 +0800

    Signed-off-by: amen <zju3020912063@gmail.com>

--- cloud/CurriculumVitaeReview.js
+++ cloud/CurriculumVitaeReview.js
@@ -3,6 +3,7 @@
  */
 var CurriculumVitae=AV.Object.extend("CurriculumVitae")
 var CurriculumVitaeReview=AV.Object.extend("CurriculumVitaeReview")
+var NotifyLog=AV.Object.extend("NotifyLog")
 function allvalues(obj)
 {
   pp=new Array()
@@ -14,10 +15,8 @@ function allvalues(obj)
 function AfterSave(request){
     var cvc=request.object
     var cv=cvc.get("cv")
-    console.log("cv:"+cv)
 
     var cvrquery=new AV.Query(CurriculumVitaeReview);
-    cvrquery.equalTo("cv",cv)
     cvrquery.find({
       success: function(results) {
           var cvrlist_len=0
@@ -51,6 +50,19 @@ function AfterSave(request){
       error: function(error) {
       }
     })
+
+    cv.fetch({
+        success:function(obcv)
+        {
+            var notify=new Array()
+            notify.push(cvc)
+            var notifyobj=new NotifyLog()
+            notifyobj.set("note",notify)
+            notifyobj.set("userObjectId",obcv.get("userObjectId"))
+            notifyobj.save()
+        }
+    })
+
 }
 
 exports.AfterSave=AfterSave
